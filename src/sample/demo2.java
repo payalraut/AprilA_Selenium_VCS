b8f742674ac73296a87e3c2cafdc6b92e5fba6a1
Asus <Asus@DESKTOP-919T09B>
2021-08-01 09:18:51 +0530

    [payal]: created new class demo2

---
+++ src/sample/demo2.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class demo2 
+{
+public static void main(String[] args) {
+	System.out.println("GM");
+	
+	System.out.println("GM");
+	
+	System.out.println("GM");
+	
+	System.out.println("GM");
+	
+	System.out.println("GM");
+	
+	System.out.println("GM");
+}
+}
